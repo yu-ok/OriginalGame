9e5560b91e30cb6e36175995d0412a3114056583
yu-ok <71647136+yu-ok@users.noreply.github.com>
2020-10-18 18:04:12 +0900

    キャラクターが動くようになりました

---
+++ Assets/RigidbodyPlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Rigidbody を使ってプレイヤーを動かすコンポーネント
+/// 入力を受け取り、それに従ってオブジェクトを動かす
+/// </summary>
+
+public class RigidbodyPlayerMovement : MonoBehaviour
+{
+    /// <summary>歩く速さ</summary>
+    [SerializeField] float m_walkSpeed = 5f;
+    /// <summary>ターンの速さ</summary>
+    [SerializeField] float m_turnSpeed = 3f;
+    
+    Rigidbody m_rb;
+    
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+        m_rb.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+    
+    void Update()
+    {
+        // 方向の入力を取得し、方向を求める
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+        
+        // 入力方向のベクトルを組み立てる
+        Vector3 dir = Vector3.forward * v + Vector3.right * h;
+        
+        // カメラを基準に入力が上下=奥/手前, 左右=左右にキャラクターを向ける
+        dir = Camera.main.transform.TransformDirection(dir);    // メインカメラを基準に入力方向のベクトルを変換する
+        dir.y = 0;  // y 軸方向はゼロにして水平方向のベクトルにする
+        dir.Normalize();    // 正規化する
+        
+        if (dir != Vector3.zero)
+        {
+            // 入力方向に滑らかに回転させる
+            Quaternion targetRotation = Quaternion.LookRotation(dir);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRotation, Time.deltaTime * m_turnSpeed);
+        }
+                
+        Vector3 velo = dir * m_walkSpeed; // 入力した方向に移動する
+        velo.y = m_rb.velocity.y;   // y 軸方向の速度を保持する
+        m_rb.velocity = velo;   // 計算した速度ベクトルをセットする
+    }
+}
