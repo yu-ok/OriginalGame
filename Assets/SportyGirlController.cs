86132274567b43b51ffb6f7b115bae109fef2dfb
yu-ok <71647136+yu-ok@users.noreply.github.com>
2020-10-18 15:56:29 +0900

    Initial commit

---
+++ Assets/SportyGirlController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class MouseMove : MonoBehaviour {
+ 
+	//　レイを飛ばす距離
+	private float rayRange = 100f;
+	//　移動する位置
+	private Vector3 targetPosition;
+	//　速度
+	private Vector3 velocity;
+	//　移動スピード
+	[SerializeField]
+	private float moveSpeed = 1.5f;
+	//　マウスクリックで移動する位置を決定するかどうか
+	[SerializeField]
+	private bool isMouseDownMode = true;
+	//　スムースにキャラクターの向きを変更するかどうか
+	[SerializeField]
+	private bool smoothRotateMode = true;
+	//　回転度合い
+	[SerializeField]
+	private float smoothRotateSpeed = 180f;
+	private CharacterController characterController;
+	private Animator animator;
+ 
+	void Start () {
+		characterController = GetComponent<CharacterController>();
+		animator = GetComponent<Animator>();
+		targetPosition = transform.position;
+		velocity = Vector3.zero;
+	}
+ 
+	void Update () {
+		if(characterController.isGrounded) {
+			velocity = Vector3.zero;
+			//　マウスクリックまたはisMouseDownModeがOffの時マウスの位置を移動する位置にする
+			if(Input.GetButton("Fire1") || !isMouseDownMode) {
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				RaycastHit hit;
+				if(Physics.Raycast(ray, out hit, rayRange, LayerMask.GetMask ("Field"))) {
+					targetPosition = hit.point;
+				}
+			}
+			//　移動の目的地と0.1mより距離がある時は速度を計算
+			if(Vector3.Distance(transform.position, targetPosition) > 0.1f) {
+				var moveDirection = (targetPosition - transform.position).normalized;
+				velocity = new Vector3(moveDirection.x * moveSpeed, velocity.y, moveDirection.z * moveSpeed);
+				//　スムースモードの時は徐々にキャラクターの向きを変更する
+				if(smoothRotateMode) {
+					transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(new Vector3(moveDirection.x, 0, moveDirection.z)), smoothRotateSpeed * Time.deltaTime);
+					//　スムースモードでなければ一気に目的地の方向を向かせる
+				} else {
+					transform.LookAt(transform.position + new Vector3(moveDirection.x, 0, moveDirection.z));
+				}
+				//　アニメーションパラメータの設定
+				animator.SetFloat("Speed", moveDirection.magnitude);
+				//　目的地に近付いたら走るアニメーションをやめる
+			} else {
+				animator.SetFloat("Speed", 0f);
+			}
+		}
+ 
+		velocity.y += Physics.gravity.y * Time.deltaTime;
+		characterController.Move(velocity * Time.deltaTime);
+	}
+}
