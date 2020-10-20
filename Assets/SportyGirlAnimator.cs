36ad54f65b3fec5799861eb16c19f1cca0ba8330
yu-ok <71647136+yu-ok@users.noreply.github.com>
2020-10-20 21:52:00 +0900

    キャラクターが動いてしまう

---
+++ Assets/SportyGirlAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SportyGirlAnimator : MonoBehaviour
+{
+        private Animator myAnimator;
+
+        // Use this for initialization
+        void Start ()
+        {
+
+                this.myAnimator = GetComponent<Animator>();
+
+                this.myAnimator.SetFloat ("Speed", 0);
+        }
+
+        void Update ()
+        {
+                if(Input.anyKey)
+                {
+                this.myAnimator.SetFloat ("Speed", 0.6f);
+                }
+                else
+                {
+                this.myAnimator.SetFloat ("Speed", 0);
+                }
+        }
+}
+
---
+++ Assets/SportyGirlController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SportyGirlController : MonoBehaviour
+{
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		
+    }
+
+   void Update () 
+   {
+	float moveHorizontal = Input.GetAxis ("Horizontal");
+	float moveVertical = Input.GetAxis ("Vertical");
+	NavMeshAgent agent = GetComponent<NavMeshAgent>();
+
+	//キャラを回転させる
+	transform.Rotate(new Vector3(0.0f, moveHorizontal, 0.0f));
+
+	//キャラの向いている方向に移動
+	agent.Move (transform.forward * moveVertical * 0.02f);
+	}
+}
